ed0a11f564cd8a01f37ebd76835826c16bce8dba
oganexon <oganexon@protonmail.com>
2019-11-30 18:54:42 +0100

    crystaux + gui

--- v2/main.js
+++ v2/main.js
@@ -6,14 +6,20 @@ import Stats from './js/stats.module.js'
 /* global requestAnimationFrame, performance */
 
 let container, stats
-let camera, scene, renderer, controls
+let camera, scene, renderer, helpers
 let geometry, material, mesh
+var raycaster = new THREE.Raycaster()
+var mouse = new THREE.Vector2()
 
 const params = {
   clipIntersection: true,
   planeConstant: 0,
   showHelpers: false,
-  density: 40
+  density: 40,
+  distribution: 'grid',
+  pause: false,
+  autoRotation: false,
+  rotationSpeed: 5
 }
 
 const clipPlanes = [
@@ -34,12 +40,15 @@ function init () {
   document.body.appendChild(container)
 
   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 5000)
-  camera.position.z = 1400
+  camera.position.z = 2000
 
   scene = new THREE.Scene()
-  controls = new OrbitControls(camera, renderer.domElement)
+  const controls = new OrbitControls(camera, renderer.domElement)
+  controls.enablePan = false
 
-  setupMesh(40)
+  setupMesh({
+    density: 40
+  })
 
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(window.innerWidth, window.innerHeight)
@@ -51,37 +60,42 @@ function init () {
   window.addEventListener('resize', onWindowResize, false)
 
   // helpers
-  var helpers = new THREE.Group()
+  helpers = new THREE.Group()
   helpers.add(new THREE.PlaneHelper(clipPlanes[0], 1100, 0xff0000))
   helpers.add(new THREE.PlaneHelper(clipPlanes[1], 1100, 0x00ff00))
   helpers.add(new THREE.PlaneHelper(clipPlanes[2], 1100, 0x0000ff))
   helpers.visible = false
   scene.add(helpers)
 
-  // gui
-  var gui = new GUI()
-  gui.add(params, 'clipIntersection').name('Découpage').onChange(function (value) {
-    mesh.material.clipIntersection = value
-  })
+  setupGUI()
+
+  return true
+}
+
+function setupGUI () {
+  const gui = new GUI({ width: 350 })
+
+  gui.add(params, 'distribution', { Grille: 'grid', Crystal: 'crystal', Aléatoire: 'random' }).name('Répartition').onChange(() => setupMesh(params))
+  gui.add(params, 'density', 2, 200).step(1).name('Densité').onChange(() => setupMesh(params))
 
   gui.add(params, 'planeConstant', -500, 500).step(10).name('Coordonnées plans').onChange(function (value) {
     for (var j = 0; j < clipPlanes.length; j++) {
       clipPlanes[j].constant = value
     }
   })
-
-  gui.add(params, 'density', 1, 200).step(1).name('Densité').onChange(function (value) {
-    setupMesh(value)
+  gui.add(params, 'clipIntersection').name('Découpage').onChange(function (value) {
+    mesh.material.clipIntersection = value
   })
-
   gui.add(params, 'showHelpers').name('Afficher les plans').onChange(function (value) {
     helpers.visible = value
   })
 
-  return true
+  gui.add(params, 'pause').name('Pause').listen()
+  gui.add(params, 'autoRotation').name('Rotation automatique').listen()
+  gui.add(params, 'rotationSpeed', 0, 10).step(1).name('Vitesse de rotation').listen()
 }
 
-function setupMesh (density, random) {
+function setupMesh ({ density, distribution = 'grid' }) {
   if (mesh) scene.remove(mesh)
 
   const circleGeometry = new THREE.CircleBufferGeometry(1, 6)
@@ -89,13 +103,24 @@ function setupMesh (density, random) {
   geometry.index = circleGeometry.index
   geometry.attributes = circleGeometry.attributes
 
-  const translateArray = random ? particlesRandom(density) : particlesGrid(density)
+  let translateArray
+  switch (distribution) {
+    case 'random':
+      translateArray = particlesRandom(density)
+      break
+    case 'crystal':
+      translateArray = particlesCrystal(density)
+      break
+    default:
+      translateArray = particlesGrid(density)
+      break
+  }
 
   geometry.setAttribute('translate', new THREE.InstancedBufferAttribute(translateArray, 3))
   material = new THREE.RawShaderMaterial({
     uniforms: {
-      'map': { value: new THREE.TextureLoader().load('circle.png') },
-      'time': { value: 0.0 }
+      map: { value: new THREE.TextureLoader().load('circle3.png') },
+      time: { value: 0.0 }
     },
     vertexShader: document.getElementById('vshader').textContent,
     fragmentShader: document.getElementById('fshader').textContent,
@@ -114,18 +139,19 @@ function setupMesh (density, random) {
 function particlesRandom (density) {
   const particleCount = density ** 3
   const translateArray = new Float32Array(particleCount * 3)
-  for (let i = 0, i3 = 0, l = particleCount; i < l; i++ , i3 += 3) {
-    translateArray[i3 + 0] = Math.random() * 2 - 1
-    translateArray[i3 + 1] = Math.random() * 2 - 1
-    translateArray[i3 + 2] = Math.random() * 2 - 1
+  for (let j = 0, i = 0, l = particleCount; j < l; j++) {
+    translateArray[i + 0] = Math.random() * 2 - 1
+    translateArray[i + 1] = Math.random() * 2 - 1
+    translateArray[i + 2] = Math.random() * 2 - 1
+    i += 3
   }
   return translateArray
 }
 
 function particlesGrid (density) {
   const particleCount = density ** 3
-  density--
   const translateArray = new Float32Array(particleCount * 3)
+  density--
 
   for (let x = 0, i3 = 0; x <= density; x++) {
     for (let y = 0; y <= density; y++) {
@@ -140,6 +166,47 @@ function particlesGrid (density) {
   return translateArray
 }
 
+function particlesCrystal (density) {
+  const particleCount = density ** 3 + (density - 1) ** 3
+  const translateArray = new Float32Array(particleCount * 3)
+  density--
+
+  let i = 0
+  for (let x = 0; x <= density; x++) {
+    for (let y = 0; y <= density; y++) {
+      for (let z = 0; z <= density; z++) {
+        translateArray[i + 0] = (x / density) * 2 - 1
+        translateArray[i + 1] = (y / density) * 2 - 1
+        translateArray[i + 2] = (z / density) * 2 - 1
+        i += 3
+      }
+    }
+  }
+
+  const offset = 0.5 / density
+  for (let x = 0; x <= density - 1; x++) {
+    for (let y = 0; y <= density - 1; y++) {
+      for (let z = 0; z <= density - 1; z++) {
+        translateArray[i + 0] = (x / density + offset) * 2 - 1
+        translateArray[i + 1] = (y / density + offset) * 2 - 1
+        translateArray[i + 2] = (z / density + offset) * 2 - 1
+        i += 3
+      }
+    }
+  }
+  return translateArray
+}
+
+window.addEventListener('mousemove', onMouseMove, false)
+
+function onMouseMove (event) {
+  // calculate mouse position in normalized device coordinates
+  // (-1 to +1) for both components
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1
+}
+
 function onWindowResize () {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
@@ -148,16 +215,21 @@ function onWindowResize () {
 
 function animate () {
   requestAnimationFrame(animate)
-  render()
   stats.update()
+  render()
 }
 
 function render () {
   const time = performance.now() * 0.0005
-  material.uniforms['time'].value = time
-  // mesh.rotation.x = time * 0.2
-  // mesh.rotation.y = time * 0.4
+  if (!params.pause) {
+    material.uniforms['time'].value = time
+  }
+  if (params.autoRotation) {
+    mesh.rotation.x += params.rotationSpeed / 100
+    mesh.rotation.y += params.rotationSpeed / 100
+  }
   renderer.render(scene, camera)
+  // console.log(raycaster.intersectObjects(scene.children))
 }
 
 if (init()) {
